f62b35f09e799e76cd79643921337772e6c08136
Rafael Römhild <rafael@roemhild.de>
2015-03-10 00:40:11 +0100

    Bump version 0.4 -> 0.5

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ from setuptools import setup
 
 setup(
     name='Flask-LDAPConn',
-    version='0.4',
+    version='0.5',
     url='http://github.com/rroemhild/flask-ldapconn',
     license='BSD',
     author='Rafael Römhild',
     author_email='rafael@roemhild.de',
     keywords='flask ldap ldap3',
-    description='Basic, pure python, LDAP connection for Flask Applications',
+    description='Pure python, LDAP connection and model for Flask Applications',
     long_description=open('README.rst').read(),
     py_modules=[
         'flask_ldapconn'
@@ -38,6 +38,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Framework :: Flask',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
