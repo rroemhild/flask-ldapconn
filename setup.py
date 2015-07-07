94faa6ed0d0da37909be1ed39211229214b3cc94
Rafael Römhild <rafael@roemhild.de>
2015-07-07 20:29:57 +0200

    Bump to version 0.6.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='Flask-LDAPConn',
-    version='0.6.2',
+    version='0.6.3',
     url='http://github.com/rroemhild/flask-ldapconn',
     license='BSD',
     author='Rafael Römhild',
